372974dafd9b9e8167b4b4a4422b9e6032c40e13
Rolando Goloya Jr <rgoloyajr@gmail.com>
2024-06-05 21:34:18 +0800

    my Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace CRUD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label_TOTAL = new System.Windows.Forms.Label();
+            this.txt_salary = new System.Windows.Forms.TextBox();
+            this.label_SALARY = new System.Windows.Forms.Label();
+            this.txt_email = new System.Windows.Forms.TextBox();
+            this.label_EMAIL = new System.Windows.Forms.Label();
+            this.txt_address = new System.Windows.Forms.TextBox();
+            this.label_ADDRESS = new System.Windows.Forms.Label();
+            this.txt_name = new System.Windows.Forms.TextBox();
+            this.label_NAME = new System.Windows.Forms.Label();
+            this.txt_id = new System.Windows.Forms.TextBox();
+            this.label_ID = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Add_btn = new System.Windows.Forms.Button();
+            this.Update_btn = new System.Windows.Forms.Button();
+            this.Delete_btn = new System.Windows.Forms.Button();
+            this.Edit_btn = new System.Windows.Forms.Button();
+            this.Editbtn = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.groupBox1.Controls.Add(this.richTextBox1);
+            this.groupBox1.Controls.Add(this.label_TOTAL);
+            this.groupBox1.Controls.Add(this.txt_salary);
+            this.groupBox1.Controls.Add(this.label_SALARY);
+            this.groupBox1.Controls.Add(this.txt_email);
+            this.groupBox1.Controls.Add(this.label_EMAIL);
+            this.groupBox1.Controls.Add(this.txt_address);
+            this.groupBox1.Controls.Add(this.label_ADDRESS);
+            this.groupBox1.Controls.Add(this.txt_name);
+            this.groupBox1.Controls.Add(this.label_NAME);
+            this.groupBox1.Controls.Add(this.txt_id);
+            this.groupBox1.Controls.Add(this.label_ID);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.Color.Chocolate;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(279, 216);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Employee Data ";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // label_TOTAL
+            // 
+            this.label_TOTAL.AutoSize = true;
+            this.label_TOTAL.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_TOTAL.ForeColor = System.Drawing.Color.Red;
+            this.label_TOTAL.Location = new System.Drawing.Point(12, 181);
+            this.label_TOTAL.Name = "label_TOTAL";
+            this.label_TOTAL.Size = new System.Drawing.Size(150, 20);
+            this.label_TOTAL.TabIndex = 10;
+            this.label_TOTAL.Text = "TOTAL SALARY :";
+            this.label_TOTAL.Click += new System.EventHandler(this.label_TOTAL_Click);
+            // 
+            // txt_salary
+            // 
+            this.txt_salary.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_salary.Location = new System.Drawing.Point(92, 143);
+            this.txt_salary.Name = "txt_salary";
+            this.txt_salary.Size = new System.Drawing.Size(177, 22);
+            this.txt_salary.TabIndex = 9;
+            this.txt_salary.TextChanged += new System.EventHandler(this.txt_salary_TextChanged);
+            // 
+            // label_SALARY
+            // 
+            this.label_SALARY.AutoSize = true;
+            this.label_SALARY.Location = new System.Drawing.Point(9, 149);
+            this.label_SALARY.Name = "label_SALARY";
+            this.label_SALARY.Size = new System.Drawing.Size(63, 13);
+            this.label_SALARY.TabIndex = 8;
+            this.label_SALARY.Text = "SALARY :";
+            // 
+            // txt_email
+            // 
+            this.txt_email.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_email.Location = new System.Drawing.Point(92, 114);
+            this.txt_email.Name = "txt_email";
+            this.txt_email.Size = new System.Drawing.Size(177, 22);
+            this.txt_email.TabIndex = 7;
+            // 
+            // label_EMAIL
+            // 
+            this.label_EMAIL.AutoSize = true;
+            this.label_EMAIL.Location = new System.Drawing.Point(9, 120);
+            this.label_EMAIL.Name = "label_EMAIL";
+            this.label_EMAIL.Size = new System.Drawing.Size(52, 13);
+            this.label_EMAIL.TabIndex = 6;
+            this.label_EMAIL.Text = "EMAIL :";
+            // 
+            // txt_address
+            // 
+            this.txt_address.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_address.Location = new System.Drawing.Point(92, 83);
+            this.txt_address.Name = "txt_address";
+            this.txt_address.Size = new System.Drawing.Size(177, 22);
+            this.txt_address.TabIndex = 5;
+            // 
+            // label_ADDRESS
+            // 
+            this.label_ADDRESS.AutoSize = true;
+            this.label_ADDRESS.Location = new System.Drawing.Point(9, 89);
+            this.label_ADDRESS.Name = "label_ADDRESS";
+            this.label_ADDRESS.Size = new System.Drawing.Size(74, 13);
+            this.label_ADDRESS.TabIndex = 4;
+            this.label_ADDRESS.Text = "ADDRESS :";
+            // 
+            // txt_name
+            // 
+            this.txt_name.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_name.Location = new System.Drawing.Point(92, 53);
+            this.txt_name.Name = "txt_name";
+            this.txt_name.Size = new System.Drawing.Size(177, 22);
+            this.txt_name.TabIndex = 3;
+            this.txt_name.TextChanged += new System.EventHandler(this.txt_name_TextChanged);
+            // 
+            // label_NAME
+            // 
+            this.label_NAME.AutoSize = true;
+            this.label_NAME.Location = new System.Drawing.Point(9, 56);
+            this.label_NAME.Name = "label_NAME";
+            this.label_NAME.Size = new System.Drawing.Size(54, 13);
+            this.label_NAME.TabIndex = 2;
+            this.label_NAME.Text = "NAME : ";
+            // 
+            // txt_id
+            // 
+            this.txt_id.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_id.Location = new System.Drawing.Point(92, 23);
+            this.txt_id.Name = "txt_id";
+            this.txt_id.Size = new System.Drawing.Size(177, 22);
+            this.txt_id.TabIndex = 1;
+            // 
+            // label_ID
+            // 
+            this.label_ID.AutoSize = true;
+            this.label_ID.Location = new System.Drawing.Point(9, 28);
+            this.label_ID.Name = "label_ID";
+            this.label_ID.Size = new System.Drawing.Size(28, 13);
+            this.label_ID.TabIndex = 0;
+            this.label_ID.Text = "ID :";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(168, 172);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(101, 38);
+            this.richTextBox1.TabIndex = 11;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(275, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(432, 261);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Add_btn
+            // 
+            this.Add_btn.BackColor = System.Drawing.Color.MediumSpringGreen;
+            this.Add_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Add_btn.Location = new System.Drawing.Point(2, 222);
+            this.Add_btn.Name = "Add_btn";
+            this.Add_btn.Size = new System.Drawing.Size(63, 39);
+            this.Add_btn.TabIndex = 2;
+            this.Add_btn.Text = "ADD";
+            this.Add_btn.UseVisualStyleBackColor = false;
+            this.Add_btn.Click += new System.EventHandler(this.Add_btn_Click);
+            // 
+            // Update_btn
+            // 
+            this.Update_btn.BackColor = System.Drawing.Color.MediumSpringGreen;
+            this.Update_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Update_btn.Location = new System.Drawing.Point(71, 222);
+            this.Update_btn.Name = "Update_btn";
+            this.Update_btn.Size = new System.Drawing.Size(62, 39);
+            this.Update_btn.TabIndex = 3;
+            this.Update_btn.Text = "UPDATE";
+            this.Update_btn.UseVisualStyleBackColor = false;
+            this.Update_btn.Click += new System.EventHandler(this.Update_btn_Click);
+            // 
+            // Delete_btn
+            // 
+            this.Delete_btn.BackColor = System.Drawing.Color.MediumSpringGreen;
+            this.Delete_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Delete_btn.Location = new System.Drawing.Point(209, 222);
+            this.Delete_btn.Name = "Delete_btn";
+            this.Delete_btn.Size = new System.Drawing.Size(60, 39);
+            this.Delete_btn.TabIndex = 4;
+            this.Delete_btn.Text = "DELETE";
+            this.Delete_btn.UseVisualStyleBackColor = false;
+            this.Delete_btn.Click += new System.EventHandler(this.Delete_btn_Click);
+            // 
+            // Edit_btn
+            // 
+            this.Edit_btn.BackColor = System.Drawing.Color.MediumSpringGreen;
+            this.Edit_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Edit_btn.Location = new System.Drawing.Point(139, 222);
+            this.Edit_btn.Name = "Edit_btn";
+            this.Edit_btn.Size = new System.Drawing.Size(62, 39);
+            this.Edit_btn.TabIndex = 5;
+            this.Edit_btn.Text = "EDIT";
+            this.Edit_btn.UseVisualStyleBackColor = false;
+            this.Edit_btn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Editbtn
+            // 
+            this.Editbtn.BackColor = System.Drawing.Color.MediumSpringGreen;
+            this.Editbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Editbtn.Location = new System.Drawing.Point(139, 222);
+            this.Editbtn.Name = "Editbtn";
+            this.Editbtn.Size = new System.Drawing.Size(62, 39);
+            this.Editbtn.TabIndex = 5;
+            this.Editbtn.Text = "EDIT";
+            this.Editbtn.UseVisualStyleBackColor = false;
+            this.Editbtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(709, 264);
+            this.Controls.Add(this.Edit_btn);
+            this.Controls.Add(this.Delete_btn);
+            this.Controls.Add(this.Update_btn);
+            this.Controls.Add(this.Add_btn);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox1);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "CRUD_CS3";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label_TOTAL;
+        private System.Windows.Forms.TextBox txt_salary;
+        private System.Windows.Forms.Label label_SALARY;
+        private System.Windows.Forms.TextBox txt_email;
+        private System.Windows.Forms.Label label_EMAIL;
+        private System.Windows.Forms.TextBox txt_address;
+        private System.Windows.Forms.Label label_ADDRESS;
+        private System.Windows.Forms.TextBox txt_name;
+        private System.Windows.Forms.Label label_NAME;
+        private System.Windows.Forms.TextBox txt_id;
+        private System.Windows.Forms.Label label_ID;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button Add_btn;
+        private System.Windows.Forms.Button Update_btn;
+        private System.Windows.Forms.Button Delete_btn;
+        private System.Windows.Forms.Button Edit_btn;
+        private System.Windows.Forms.Button Editbtn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Xml.Linq;
+using System.IO;
+
+namespace CRUD
+{
+    public partial class Form1 : Form
+    {
+        private XDocument xmldoc;
+        private string xmlfile = "EmpDataBased.xml";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label_TOTAL_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                xmldoc = XDocument.Load(xmlfile);
+                var data = xmldoc.Descendants("Employee").Select(p => new
+                {
+                    id = p.Element("id").Value,
+                    name = p.Element("name").Value,
+                    address = p.Element("address").Value,
+                    email = p.Element("email").Value,
+                    salary = p.Element("salary").Value,
+                }).OrderBy(p => p.id).ToList();
+                dataGridView1.DataSource = data;
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+        }
+
+        private void Add_btn_Click(object sender, EventArgs e)
+        {
+            XElement emp = new XElement("Employee",
+                      new XElement("id", txt_id.Text),
+                      new XElement("name", txt_name.Text),
+                      new XElement("salary", txt_salary.Text),
+                      new XElement("email", txt_email.Text),
+                      new XElement("address", txt_address.Text));
+            xmldoc.Root.Add(emp);
+            xmldoc.Save(xmlfile);
+        }
+        private void Update_btn_Click(object sender, EventArgs e)
+        {
+            if (txt_id.Text != "" & txt_name.Text != "" & txt_salary.Text != "" & txt_email.Text != "" & txt_address.Text != "")
+            {
+                XElement emp = xmldoc.Descendants("Employee").FirstOrDefault(p => p.Element("id").Value == txt_id.Text);
+                if (emp != null)
+                {
+                    emp.Element("fullname").Value = txt_name.Text;
+                    emp.Element("salary").Value = txt_salary.Text;
+                    emp.Element("email").Value = txt_email.Text;
+                    emp.Element("address").Value = txt_address.Text;
+                    xmldoc.Save(xmlfile);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please Fully Fill up the Form", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Delete_btn_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                foreach (DataGridViewRow row in dataGridView1.SelectedRows)
+                {
+                    txt_id.Text = row.Cells[0].Value.ToString();
+                    txt_name.Text = row.Cells[1].Value.ToString();
+                    txt_salary.Text = row.Cells[2].Value.ToString();
+                    txt_email.Text = row.Cells[3].Value.ToString();
+                    txt_address.Text = row.Cells[4].Value.ToString();
+                }
+                if (dataGridView1.SelectedRows.Count > 0)
+                {
+                    XElement emp = xmldoc.Descendants("Employee").FirstOrDefault(p => p.Element("id").Value == txt_id.Text);
+                    Console.WriteLine(emp);
+                    if (emp != null)
+                    {
+                        emp.Remove();
+                        xmldoc.Save(xmlfile);
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                foreach (DataGridViewRow row in dataGridView1.SelectedRows)
+                {
+                    txt_id.Text = row.Cells[0].Value.ToString();
+                    txt_name.Text = row.Cells[1].Value.ToString();
+                    txt_salary.Text = row.Cells[2].Value.ToString();
+                    txt_email.Text = row.Cells[3].Value.ToString();
+                    txt_address.Text = row.Cells[4].Value.ToString();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a row", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_name_TextChanged(object sender, EventArgs e)
+        {
+           // e.Handled = !(char.IsLetter(e.KeyChar) || e.KeyChar == (char)Keys.Back || e.KeyChar == (char)Keys.Space);
+        }
+
+        private void txt_salary_TextChanged(object sender, EventArgs e)
+        {
+         //   e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == 0 && dataGridView1.CurrentCell.Value != null)
+            {
+
+            }
+        }
+    }
+}
